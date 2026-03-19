2daa27f497f3a35f40c16a9aa54d382ba087388f
Aengus MacKenzie <aengus.2000@gmail.com>
2026-03-19 15:19:44 -0400

    finished consecutive substrings lab

--- consecutive_substrings.js
+++ consecutive_substrings.js
@@ -1,5 +1,13 @@
 function consecutiveSubstrings(string) {
   // type your code here
+    const substrings = [];
+
+    for (let i = 0; i < string.length; i++) {
+      for (let j = i + 1; j <= string.length; j++) {
+          substrings.push(string.substring(i, j));
+      }
+    }
+    return substrings;
 }
 
 if (require.main === module) {
@@ -11,6 +19,21 @@ if (require.main === module) {
 
   console.log("Expecting: ['a']");
   console.log("=>", consecutiveSubstrings('a'));
+
+  console.log("");
+
+  console.log("Expecting: ['h', 'hi', 'i']");
+  console.log("=>", consecutiveSubstrings('hi'));
+
+  console.log("");
+
+  console.log("Expecting: ['d', 'do', 'dog', 'o', 'og', 'g']");
+  console.log("=>", consecutiveSubstrings('dog'));
+
+  console.log("");
+
+  console.log("Expecting: ['a', 'aa', 'aaa', 'aaaa', 'a', 'aa', 'aaa', 'a', 'aa', 'a']");
+  console.log("=>", consecutiveSubstrings('aaaa'));
 }
 
 module.exports = consecutiveSubstrings;
